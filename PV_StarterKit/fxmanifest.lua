36d4e07dd4a1b149362dd09ca504e1e7d982c644
Privalo21 <160001285+TTVPrivalo21@users.noreply.github.com>
2026-02-17 00:36:21 -0300

    Update fxmanifest.lua

--- PV_StarterKit/fxmanifest.lua
+++ PV_StarterKit/fxmanifest.lua
@@ -32,4 +32,17 @@ dependencies {
     'ox_lib',
     'ox_target',
     'ox_inventory'
+
 }
+
+
+
+--[[
+
+CREATE TABLE IF NOT EXISTS `starterpack_history` (
+  `citizenid` varchar(50) NOT NULL,
+  `claimed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+]]
