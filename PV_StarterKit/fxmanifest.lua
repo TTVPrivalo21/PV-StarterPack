dc3ef8820c8903eb415f49f9e7a76d63be0dd019
Privalo21 <160001285+TTVPrivalo21@users.noreply.github.com>
2026-02-23 05:38:35 -0300

    Update fxmanifest.lua

--- PV_StarterKit/fxmanifest.lua
+++ PV_StarterKit/fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+author 'PV Scripts'
+description 'PV Kit Inicial'
 version '1.0.0'
 
 shared_scripts {
@@ -34,15 +36,3 @@ dependencies {
     'ox_inventory'
 
 }
-
-
-
---[[
-
-CREATE TABLE IF NOT EXISTS `starterpack_history` (
-  `citizenid` varchar(50) NOT NULL,
-  `claimed_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`citizenid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-]]
